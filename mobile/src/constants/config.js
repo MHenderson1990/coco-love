3809e000609f4188f398dbbe25dcb6ee457d0545
MHenderson1990 <morganhenderson_90@yahoo.com>
2026-08-14 13:46:14 -0500

    Add api to the end of cloud url in config

--- mobile/src/constants/config.js
+++ mobile/src/constants/config.js
@@ -1,3 +1,3 @@
 // change this when your LAN IP changes
-export let API_URL = 'https://house-of-love-backend-392419563019.us-central1.run.app';
+export let API_URL = 'https://house-of-love-backend-392419563019.us-central1.run.app/api';
 export let CALENDLY_URL = 'https://calendly.com/';
