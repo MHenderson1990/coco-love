3162655452cf4ae0fa42752ad153c5e65649e53b
MHenderson1990 <morganhenderson_90@yahoo.com>
2026-09-14 12:50:08 -0500

    resize widget photo

--- mobile/widgets/TodayAffirmationWidget.jsx
+++ mobile/widgets/TodayAffirmationWidget.jsx
@@ -10,8 +10,8 @@ export function TodayAffirmationWidget({ text, photoKey }) {
     <OverlapWidget style={{ height: 'match_parent', width: 'match_parent' }}>
         <ImageWidget
         image={imageSource}
-        imageWidth={400}
-        imageHeight={200}
+        imageWidth={390}
+        imageHeight={260}
         resizeMode="cover"
         style={{ height: 'match_parent', width: 'match_parent' }}
       />
